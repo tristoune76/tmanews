b46d410977f6251cac86668666af4b99b5250343
tristan <tristan.misseri@netc.fr>
2020-08-31 10:09:08 +0200

    3ème commit

---
+++ lib/OCFram/route.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace OCFram;
+
+class Route
+{
+    private $action;
+    private $module;
+    private $url;
+    private $varNames;
+    private $vars = [];
+
+    public function __construct ($action, $module, $url, $varNames, $vars)
+    {
+        $this->setAction($action);
+        $this->setModule($module);
+        $this->setUrl($url);
+        $this->setVarNames($varNames);
+        
+    }
+
+    public function setAction($action)
+    {
+        if (is_string($action))
+        {
+            $this->action = $action;
+        }
+    }
+
+    public function setModule($module)
+    {
+        if (is_string($module))
+        {
+            $this->module = $module;
+        }
+    }   
+
+    public function setVarNames($url)
+    {
+        $this->varNames = $varNames;
+    }
+
+    public function setVars(array $vars)
+    {
+        $this->vars = $vars;
+    }
+
+    public function hasVars ()
+    {
+        return !empty($this->varsNames);
+    }
+
+    public function match ($url)
+    {
+        if(preg_match("`^".$this->url."$`",$url,$matches))
+        {
+            return $matches;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public function url() {return $this->url;}
+    public function action() {return $this->action;}
+    public function module() {return $this->module;}
+    public function varNames() {return $this->varNames;}
+    public function vars() {return $this->vars;}
+}
---
+++ lib/OCFram/router.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace OCFram;
+
+class Router
+
+{
+    private $routes;
+
+    const NO_ROUTE = 1;
+
+    public function addRoute (Route $route)
+    {
+        if (-in_array($route, $this->routes))
+        {
+            $this->routes[] = $route;
+        }
+    }
+
+    public function getRoute ($url)
+    {
+        foreach( $this->routes as $route)
+        {
+            //if the road corresponds to url
+            if(($varsValues = $route->match($url)) !== false)
+            {
+                
+                //if the route has variable
+                if($route->hasVars())
+                {
+                    $varsNames = $route->varsNames();
+                    $listVars=[];
+
+                    //we are creating a news key/value array
+                    //key = name of the variable and value = value of the variable
+                    foreach ($varNames as $key -> $value)
+                    {
+                        //when using preg_match the first line of the array is not usefull (see preg_match manual)
+                        if($key !==0)
+                        {
+                            //fulling the array with the variable
+                            $listVars[$varNames[$key-1]] = $value;
+                        }
+                    }
+                    
+                    //setting the vars variable of the route with the array
+                    $route->setVars($listVars);
+                }
+                return $route;
+            }
+        }
+        throw new \RuntimeException('No road for this url', self::NO_ROUTE);
+    }
+}
